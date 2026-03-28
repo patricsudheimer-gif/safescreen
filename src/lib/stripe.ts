a648de865f67b04f29c896bbb5f42c37b460f9b3
Patric Sudheimer <patric.sudheimer@googlemail.com>
2026-03-28 15:41:37 +0100

    Fix Stripe API version for Vercel build

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,11 +1,7 @@
 import Stripe from 'stripe'
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY ist nicht gesetzt')
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 })
 
